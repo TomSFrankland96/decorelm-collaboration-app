c3f83a2e61bcbff425b90be2043a030e4ff159dc
Tom Frankland <tom.frankland@aol.com>
2023-03-16 23:25:41 +0000

    fixing cloudinary issues

--- models/design.js
+++ models/design.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String
+});
+
 const opts = { toJSON: { virtuals: true } };
 
 const DesignSchema = new Schema({
   name: String,
   document: String,
+  image: [ImageSchema],
   datePosted: String
 }, opts);
 
